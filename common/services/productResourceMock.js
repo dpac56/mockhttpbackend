65e8308e8f49e171b13111f78ee25e7e75346eee
deepak <deepak@withstartups.com>
2014-10-26 21:08:56 +0530

    Working routes and menu

--- common/services/productResourceMock.js
+++ common/services/productResourceMock.js
@@ -58,8 +58,46 @@
 		];
 
 		var productUrl = "/api/products"
+		var editingRegex = new RegExp(productUrl + "/[0-9][0-9]*", '');
+
 		$httpBackend.whenGET(productUrl).respond(products);
 
+		$httpBackend.whenGET(editingRegex).respond(function(method, url, data){
+			var product = {'productId': 0};
+			var parameters = url.split('/');
+			var length = parameters.length;
+			var id = parameters[length - 1];
+
+			if (id > 0){
+				for (var i = 0; i < products.length; i++) {
+					if (products[i].productId == id){
+						product = products[i];
+						break;
+					}
+				};
+			}
+			return [200, product, {}];
+		});
+
+		$httpBackend.whenPOST(productUrl).respond(function (method, url, data) {
+			var product = angular.fromJson(data);
+
+			if (!product.productId) {
+				product.productId = product[products.length - 1].productId + 1;
+				products.push(product);
+			}else 
+				{
+				for (var i = 0; i < products.length; i++) {
+					if (products[i].productId == product.productId){
+						products[1] = product;
+						break;
+					}
+				};
+			}
+			return [200, product, {}];
+		});
+
+		$httpBackend.whenGET(/app/).passThrough();
 
 	});
 
